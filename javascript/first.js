563aaa437162c3958bd0c75fc9b6f2a6cc29f86e
Rushil Choudhary <choudharyrushil118@gmail.com>
2023-12-01 23:11:41 +0530

    ready to deploy

---
+++ javascript/first.js
@@ -0,0 +1,51 @@
+// console.log("Hello World")
+// var num = 10;
+// console.log(num)
+
+// var ch1 = "a"
+// console.log(ch1)
+
+// var b = true
+// console.log(b)
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// while (num > 0) {
+//     console.log(num)
+//     num--
+// }
+
+// var arr = []
+// arr.push("1")
+// arr.push("second")
+// arr.push("3")
+
+// console.log(arr)
+
+// arr.unshift("first")
+
+// console.log(arr)
+
+// arr.pop("1");
+
+// console.log(arr)
+
+// arr.shift()
+
+// console.log(arr)
+
+// arr.splice(0,0,"me")
+
+// console.log(arr)
+
+// arr.slice(1)
+
+// console.log(arr)
+
+var str = "Hello I am"
+arr = str.split(" ")
+console.log(arr)
+var strr = arr.join("/")
+console.log(strr)
